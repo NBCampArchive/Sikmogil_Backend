b934cb8d37918bc4787302239efb136be3071870
giwoong <dev.choi0409@gmail.com>
2024-06-02 12:30:37 +0900

    first commit

---
+++ src/main/java/com/examle/sikmogilbackend/SikmogilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examle.sikmogilbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikmogilBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SikmogilBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examle/sikmogilbackend/SikmogilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examle.sikmogilbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikmogilBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
