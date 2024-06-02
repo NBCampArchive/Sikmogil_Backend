ec62ed62900801b6c69fac0f8a73ae2fc8490f42
giwoong <dev.choi0409@gmail.com>
2024-06-02 15:55:59 +0900

    feat: 온보딩 정보 입력 api 구현

---
+++ src/main/java/com/examle/sikmogilbackend/member/api/dto/reqeust/OnboardingInfoUpdateReqDto.java
@@ -0,0 +1,11 @@
+package com.examle.sikmogilbackend.member.api.dto.reqeust;
+
+public record OnboardingInfoUpdateReqDto(
+        String nickname,
+        String height,
+        String weight,
+        String gender,
+        String targetWeight,
+        String targetDate
+) {
+}
--- src/main/java/com/examle/sikmogilbackend/member/domain/Member.java
+++ src/main/java/com/examle/sikmogilbackend/member/domain/Member.java
@@ -1,5 +1,6 @@
 package com.examle.sikmogilbackend.member.domain;
 
+import com.examle.sikmogilbackend.member.api.dto.reqeust.OnboardingInfoUpdateReqDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -37,6 +38,24 @@ public class Member {
     @Schema(description = "소셜로그인 타입", example = "GOOGLE, APPLE")
     private SocialType socialType;
 
+    @Schema(description = "사용자가 설정한 닉네임", example = "홍홍홍")
+    private String nickname;
+
+    @Schema(description = "키", example = "180")
+    private String height;
+
+    @Schema(description = "몸무게", example = "90")
+    private String weight;
+
+    @Schema(description = "성별", example = "남자, 여자")
+    private String gender;
+
+    @Schema(description = "목표 몸무게", example = "75")
+    private String targetWeight;
+
+    @Schema(description = "목표 날짜", example = "2024.06.02")
+    private String targetDate;
+
     @Enumerated(EnumType.STRING)
     @Schema(description = "권한", example = "ROLE_USER")
     private Role role;
@@ -48,5 +67,14 @@ public class Member {
         this.picture = picture;
         this.socialType = socialType;
         this.role = role;
+    }
+
+    public void onboardingUpdate(OnboardingInfoUpdateReqDto onboardingInfoUpdateReqDto) {
+        this.nickname = onboardingInfoUpdateReqDto.nickname();
+        this.height = onboardingInfoUpdateReqDto.height();
+        this.weight = onboardingInfoUpdateReqDto.weight();
+        this.gender = onboardingInfoUpdateReqDto.gender();
+        this.targetWeight = onboardingInfoUpdateReqDto.targetWeight();
+        this.targetDate = onboardingInfoUpdateReqDto.targetDate();
     }
 }
