29c6daffe73c0dae68ed766ed9fb3f9594879e79
giwoong <dev.choi0409@gmail.com>
2024-06-02 14:17:19 +0900

    feat: Member domain 구현

---
+++ src/main/java/com/examle/sikmogilbackend/member/domain/Member.java
@@ -0,0 +1,45 @@
+package com.examle.sikmogilbackend.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    private String email;
+
+    private String name;
+
+    private String picture;
+
+    @Enumerated(value = EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    private Member(String email, String name, String picture, SocialType socialType, Role role) {
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.socialType = socialType;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/examle/sikmogilbackend/member/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.examle.sikmogilbackend.member.domain.repository;
+
+import com.examle.sikmogilbackend.member.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+}
