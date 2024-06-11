d573d896e48b9a00568dca03bc99e8fc8854878a
LEEHYUNBOK <l.hn.bk0905@gmail.com>
2024-06-11 23:17:34 +0900

    Feat:이미지 등록 api 생성

---
+++ src/main/java/com/examle/sikmogilbackend/gcs/GoogleCloudStorageConfig.java
@@ -0,0 +1,34 @@
+package com.examle.sikmogilbackend.gcs;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class GoogleCloudStorageConfig {
+    @Value("${myapp.local-url}")
+    private String localUrl;
+    @Value("${spring.cloud.project-id}") // application.yml에 써둔 bucket 이름
+    private String projectId;
+
+    @Value("${spring.cloud.gcp.storage.credentials.location}") // application.yml에 써둔 bucket 이름
+    private String keyJson;
+
+    @Bean
+    public Storage storage() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource(keyJson);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+        return StorageOptions.newBuilder()
+                .setProjectId(projectId)
+                .setCredentials(credentials)
+                .build()
+                .getService();
+    }
+}
