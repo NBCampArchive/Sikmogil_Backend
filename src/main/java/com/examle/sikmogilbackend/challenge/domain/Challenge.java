3963b981bd9a2c67c4111e0546aa0d6cdd87aef3
giwoong <dev.choi0409@gmail.com>
2024-06-30 03:03:47 +0900

    Feat(#27): 챌린지 도메인 구현

---
+++ src/main/java/com/examle/sikmogilbackend/challenge/domain/Challenge.java
@@ -0,0 +1,67 @@
+package com.examle.sikmogilbackend.challenge.domain;
+
+import com.examle.sikmogilbackend.member.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Challenge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "challenge_id")
+    @Schema(description = "챌린지 그룹아이디")
+    private Long challengeId;
+
+    @OneToOne
+    @Schema(description = "챌린지 그룹장")
+    private Member leader;
+
+    @Schema(description = "챌린지 그룹 제목")
+    private String topic;
+
+    @Schema(description = "챌린지 그룹 이름")
+    private String name;
+
+    @Schema(description = "챌린지 그룹 소개")
+    private String introduction;
+
+    @Schema(description = "챌린지 그룹 성별")
+    private String gender;
+
+    @Schema(description = "챌린지 그룹 활동 정도")
+    private String activityLevel;
+
+    @Schema(description = "챌린지 그룹 참여자 목록")
+    @OneToMany(mappedBy = "challenge", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Member> participantList = new ArrayList<>();
+
+    @Builder
+    private Challenge(Member leader, String topic, String name, String introduction, String gender,
+                      String activityLevel,
+                      List<Member> participantList) {
+        this.leader = leader;
+        this.topic = topic;
+        this.name = name;
+        this.introduction = introduction;
+        this.gender = gender;
+        this.activityLevel = activityLevel;
+        this.participantList = participantList;
+    }
+
+}
