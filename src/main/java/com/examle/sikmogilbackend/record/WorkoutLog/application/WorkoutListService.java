885716de63106424bd2a61cc83a27a2ba5d322c1
giwoong <dev.choi0409@gmail.com>
2024-07-01 13:31:02 +0900

    Fix(#13): 운동기록 사진만 삭제 api 구현

--- src/main/java/com/examle/sikmogilbackend/record/WorkoutLog/application/WorkoutListService.java
+++ src/main/java/com/examle/sikmogilbackend/record/WorkoutLog/application/WorkoutListService.java
@@ -2,28 +2,22 @@ package com.examle.sikmogilbackend.record.WorkoutLog.application;
 
 import com.examle.sikmogilbackend.member.application.MemberService;
 import com.examle.sikmogilbackend.member.domain.Member;
-import com.examle.sikmogilbackend.member.domain.repository.MemberRepository;
 import com.examle.sikmogilbackend.record.Calendar.application.CalendarService;
 import com.examle.sikmogilbackend.record.Calendar.domain.Calendar;
 import com.examle.sikmogilbackend.record.WorkoutLog.api.dto.FindWorkoutPictureDTO;
 import com.examle.sikmogilbackend.record.WorkoutLog.api.dto.WorkoutListDTO;
-import com.examle.sikmogilbackend.record.WorkoutLog.api.dto.WorkoutLogDTO;
 import com.examle.sikmogilbackend.record.WorkoutLog.domain.WorkoutList;
 import com.examle.sikmogilbackend.record.WorkoutLog.domain.WorkoutLog;
 import com.examle.sikmogilbackend.record.WorkoutLog.domain.repository.WorkoutListRepository;
 import com.examle.sikmogilbackend.record.WorkoutLog.exception.WorkoutListNotEqualsException;
 import com.examle.sikmogilbackend.record.WorkoutLog.exception.WorkoutListNotFoundException;
-import com.examle.sikmogilbackend.record.dietLog.api.dto.FindDietPictureDTO;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -37,23 +31,25 @@ public class WorkoutListService {
     private final MemberService memberService;
 
     @Transactional
-    public List<WorkoutListDTO> findWorkoutListByDate (String email, String date) {
+    public List<WorkoutListDTO> findWorkoutListByDate(String email, String date) {
         log.info("findWorkoutListByDate ");
-        log.info("email = "+email);
-        log.info("date = "+date);
+        log.info("email = " + email);
+        log.info("date = " + date);
         Calendar calendar = calendarService.findCalendarByDiaryDate(email, date);
         WorkoutLog workoutLog = workoutLogService.findWorkoutLogByWorkoutDate(email, date);
 
-        List<WorkoutList> workoutLists = workoutListRepository.findWorkoutListsByCalendarAndWorkoutLog(calendar, workoutLog);
+        List<WorkoutList> workoutLists = workoutListRepository.findWorkoutListsByCalendarAndWorkoutLog(calendar,
+                workoutLog);
 
         return workoutLists.stream()
                 .map(WorkoutList::toDTO)
                 .collect(Collectors.toList());
     }
+
     @Transactional
-    public List<WorkoutListDTO> findWorkoutListByDate (Calendar calendar) {
+    public List<WorkoutListDTO> findWorkoutListByDate(Calendar calendar) {
         log.info("findWorkoutListByDate ");
-        log.info("calendar = "+calendar);
+        log.info("calendar = " + calendar);
         List<WorkoutList> workoutLists = workoutListRepository.findWorkoutListsByCalendar(calendar);
         return workoutLists.stream()
                 .map(WorkoutList::toDTO)
@@ -63,27 +59,28 @@ public class WorkoutListService {
     @Transactional
     public FindWorkoutPictureDTO findWorkoutPictures(String email, Integer page) {
         log.info("findWorkoutPictures ");
-        log.info("email = "+email);
+        log.info("email = " + email);
         Member member = memberService.findMember(email);
         Integer total = member.getCalendars().stream()
                 .flatMap(calendar -> calendar.getWorkoutLists().stream())
                 .filter(workoutList -> workoutList.getWorkoutPicture() != null)
                 .collect(Collectors.toList()).size();
         PageRequest pageable = PageRequest.of(page, 10);
-        List<WorkoutListDTO> workoutListDTOS= workoutListRepository.findWorkoutListByByEmailAndWorkoutPictureNotNull(email, pageable).stream()
-                                                    .map(WorkoutList::toDTO)
-                                                    .collect(Collectors.toList());
+        List<WorkoutListDTO> workoutListDTOS = workoutListRepository.findWorkoutListByByEmailAndWorkoutPictureNotNull(
+                        email, pageable).stream()
+                .map(WorkoutList::toDTO)
+                .collect(Collectors.toList());
         return FindWorkoutPictureDTO.builder()
-                .lastPage(total/10)
+                .lastPage(total / 10)
                 .pictures(workoutListDTOS)
                 .build();
     }
 
     @Transactional
-    public void addWorkoutList(String email, String date, WorkoutListDTO workoutListDTO){
+    public void addWorkoutList(String email, String date, WorkoutListDTO workoutListDTO) {
         log.info("사진 추가 중");
-        log.info("email = "+email);
-        log.info("date = "+date);
+        log.info("email = " + email);
+        log.info("date = " + date);
         Calendar calendar = calendarService.findCalendarByDiaryDate(email, date);
         WorkoutLog workoutLog = workoutLogService.findWorkoutLogByWorkoutDate(email, date);
         WorkoutList workoutList = WorkoutList.builder()
@@ -101,13 +98,23 @@ public class WorkoutListService {
     @Transactional
     public void deleteWorkoutList(String email, String date, Long workoutListId) {
         log.info("사진 삭제 중");
-        log.info("email = "+email);
-        log.info("date = "+date);
+        log.info("email = " + email);
+        log.info("date = " + date);
         WorkoutLog workoutLog = workoutLogService.findWorkoutLogByWorkoutDate(email, date);
         WorkoutList workoutList = workoutListRepository.findWorkoutListByWorkoutListId(workoutListId)
                 .orElseThrow(WorkoutListNotFoundException::new);
         checkEqualsWorkout(workoutLog, workoutList);
         workoutListRepository.delete(workoutList);
+    }
+
+    // workoutList의 사진만 삭제!
+    @Transactional
+    public void deleteOnlyWorkoutListPicture(String email, String date, Long workoutListId) {
+        WorkoutLog workoutLog = workoutLogService.findWorkoutLogByWorkoutDate(email, date);
+        WorkoutList workoutList = workoutListRepository.findWorkoutListByWorkoutListId(workoutListId)
+                .orElseThrow(WorkoutListNotFoundException::new);
+        checkEqualsWorkout(workoutLog, workoutList);
+        workoutList.deleteWorkoutPicture();
     }
 
     @Transactional
